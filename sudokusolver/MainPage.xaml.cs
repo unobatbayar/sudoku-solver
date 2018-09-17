26c8227c8c3d6dcb4a5fab054f00b4c396f0561b
Uno <unobatbayar@yahoo.com>
2018-09-17 12:42:22 +0100

    Add files via upload

---
+++ sudokusolver/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace sudokusolver
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void Handle_Pressed(object sender, System.EventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ sudokusolver/SudokuPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace sudokusolver
+{
+    public partial class SudokuPage : ContentPage
+    {
+        public SudokuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
